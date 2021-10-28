303fc493a46f60eb6bf1afe37cc87049307f88c6
wanghongtao <49244238+wanghongtaonus@users.noreply.github.com>
2021-10-29 04:47:22 +0800

    Update main.py
    1. fix bug that user need to input at least id or name, else will generate a prompt dialog
    2. add seachbyusername to get the symptoms and illness
    3.  based on the requirment change  the number 3 we can get symptoms

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ from PyQt5.QtGui import  QPixmap,QScreen
 from PyQt5.QtWidgets import (QApplication, QSplitter, QGridLayout, QHBoxLayout, QPushButton, 
                             QTreeWidget, QFrame, QLabel, QHBoxLayout, QMainWindow,
                             QStackedLayout, QWidget, QVBoxLayout, QLineEdit, QRadioButton,
-                            QTreeWidgetItem, QDesktopWidget)
+                            QTreeWidgetItem, QDesktopWidget,QMessageBox)
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
@@ -40,13 +40,13 @@ class MainWindow(QMainWindow):
         self.status.showMessage("i am at main page～")
 
         # set window size
-        self.resize(600, 400)
+        self.resize(1000, 800)
 
         # initial window located at center
-        self.center()
+        # self.center()
 
         # set the window opacity
-        self.setWindowOpacity(0.9) 
+        # self.setWindowOpacity(0.9) 
 
         # set windows style
         self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
@@ -63,15 +63,15 @@ class MainWindow(QMainWindow):
 
         # search　button
         search_btn = QPushButton(top_left_frame)
-        search_btn.setFixedSize(100, 30), search_btn.setText("search")
+        search_btn.setFixedSize(200, 60), search_btn.setText("search")
         button_layout.addWidget(search_btn)
         # view　button
         friend_btn = QPushButton(top_left_frame)
-        friend_btn.setFixedSize(100, 30), friend_btn.setText("view")
+        friend_btn.setFixedSize(200, 60), friend_btn.setText("view")
         button_layout.addWidget(friend_btn) 
         # exit button
         quit_btn = QPushButton(top_left_frame)
-        quit_btn.setFixedSize(100, 30), quit_btn.setText("exit")
+        quit_btn.setFixedSize(200, 60), quit_btn.setText("exit")
         button_layout.addWidget(quit_btn)
         
         # 左下角为空白 必须要有布局，才可以显示至内容中
@@ -79,7 +79,7 @@ class MainWindow(QMainWindow):
         blank_label = QLabel(bottom_left_frame)
         blank_layout = QVBoxLayout(bottom_left_frame)
         blank_label.setText("illness for doctor")
-        blank_label.setFixedHeight(20)
+        blank_label.setFixedHeight(40)
         blank_layout.addWidget(blank_label)
         self.webEngineView = QWebEngineView(bottom_left_frame)
         self.webEngineView.close()
@@ -96,18 +96,18 @@ class MainWindow(QMainWindow):
         self.search_id = QLineEdit(right_frame)
         self.search_id.setPlaceholderText("please input search id for patient：")
         self.search_id.setFixedWidth(400)
-        search_name = QLineEdit(right_frame)
-        search_name.setPlaceholderText("please input search name for patient ：")
-        search_name.setFixedWidth(400)
+        self.search_name = QLineEdit(right_frame)
+        self.search_name.setPlaceholderText("please input search name for patient ：")
+        self.search_name.setFixedWidth(400)
         
         
         search_confirm_btn = QPushButton("submit")
-        search_confirm_btn.setFixedSize(100, 30)
+        search_confirm_btn.setFixedSize(200, 60)
         search_layout = QVBoxLayout()
         search_widget = QWidget(right_frame)
         search_widget.setLayout(search_layout)
         search_layout.addWidget(self.search_id)
-        search_layout.addWidget(search_name)
+        search_layout.addWidget(self.search_name)
         
         
         search_layout.addWidget(search_confirm_btn)
@@ -117,15 +117,15 @@ class MainWindow(QMainWindow):
         self.friend_tree = QTreeWidget(right_frame)
         self.friend_tree.setColumnCount(5)  # 一列 
         self.friend_tree.setHeaderLabels(['name', 'id', 'symptoms','illness','threapy']) # 设置标题
-        root = QTreeWidgetItem(self.friend_tree) # 设置根节点
-        self.friend_tree.setColumnWidth(2, 400) # 设置宽度s
+        self.root = QTreeWidgetItem(self.friend_tree) # 设置根节点
+        self.friend_tree.setColumnWidth(4, 300) # 设置宽度s
         # 设置子节点
-        root.setText(0, "name") # 0 表示位置
-        root.setText(1, "id")
-        root.setText(2, "symptoms")
-        root.setText(3, "illness")
-        root.setText(4, "网址")
-        self.child_name = QTreeWidgetItem(root)
+        # root.setText(0, "name") # 0 表示位置
+        # root.setText(1, "id")
+        # root.setText(2, "symptoms")
+        # root.setText(3, "illness")
+        # root.setText(4, "threapy")
+        # self.child_name = QTreeWidgetItem(root)
         
         # self.child_name.setText(0, "name")
 
@@ -171,7 +171,15 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
         # 函数功能区
+        # while self.search_id.text() == '' and self.search_name.text()== '':
+        #     msg = QMessageBox()
+        #     msg.setText("At least input name or id")
+        #     break
+        # if self.search_id.text() == '' and self.search_name.text()== '':
+        # search_confirm_btn.clicked.connect(self.show_dialog_page)
+        # else:
         search_btn.clicked.connect(self.show_search_page)
+            
         search_confirm_btn.clicked.connect(self.show_friend_page)
         friend_btn.clicked.connect(self.show_friend_page)
         self.friend_tree.clicked.connect(self.show_firend_web)
@@ -181,10 +189,16 @@ class MainWindow(QMainWindow):
         # 刚开始要管理浏览器，否则很丑
         self.webEngineView.close()
         # 注意先后顺序，resize　在前面会使代码无效
-        self.splitter1.setMinimumWidth(150)
-        self.splitter2.setMinimumWidth(250)
-        self.resize(600, 400)
+        self.splitter1.setMinimumWidth(300)
+        self.splitter2.setMinimumWidth(500)
+        self.resize(1000, 800)
 
+    # def show_dialog_page(self):
+    #     reply = QMessageBox.warning(self, 'warning', 'u need to input',QMessageBox.Yes)
+    #     if reply == QMessageBox.Yes:
+    #         print('yes')
+        # msg.setText("At least input name or id")
+        
     # TreeView 的点击事件
     def show_firend_web(self):
         item = self.friend_tree.currentItem()
@@ -197,46 +211,60 @@ class MainWindow(QMainWindow):
 
     # display tree structure
     def show_friend_page(self):
-        print(self.search_id.text())
-        all_symptoms=fm.search_by_id(int(self.search_id.text()))
-        print(all_symptoms)
-        pre_symptoms=[]
-        for v in list(all_symptoms.keys())[2:134]:
-            if(all_symptoms[v]==1 ):
-                pre_symptoms.append(v)
-        name=all_symptoms['name']
-        id=all_symptoms['id']
-        print(pre_symptoms)
-        illness=all_symptoms['illness']
-        self.child_name.setText(0, str(name))
-        self.child_name.setText(1, str(id))
-        self.child_name.setText(2,str(pre_symptoms))
-        self.child_name.setText(3,str(illness))
-        self.child_name.setText(4,"https://www.cup.edu.cn/medical/disease/commondisease/16419.htm")
-        primary_screen = QApplication.primaryScreen()
-        sshot = primary_screen.grabWindow(0)
-        sshot.save('sshot.pdf')
-        self.init()
-        self.center()
-        self.right_layout.setCurrentIndex(1)
+        
+        if self.search_id.text() == '' and self.search_name.text()== '':
+            reply = QMessageBox.warning(self, 'warning', 'You need to input id or name',QMessageBox.Yes)
+            if reply == QMessageBox.Yes:
+                print('yes')
+            all_symptoms=None
+        elif self.search_id.text() == '' :
+            all_symptoms= fm.search_by_name(self.search_name.text())[0]
+        else:
+            all_symptoms=fm.search_by_id(int(self.search_id.text()))
+        if(all_symptoms!=None):
+            print(all_symptoms)
+            pre_symptoms=[]
+            for v in list(all_symptoms.keys())[2:134]:
+                if(all_symptoms[v]==3 ):
+                    pre_symptoms.append(v)
+            name=all_symptoms['name']
+            id=all_symptoms['id']
+            print(pre_symptoms)
+            illness=all_symptoms['illness']
+            self.root.setText(0, str(name)) # 0 表示位置
+            self.root.setText(1, str(id))
+            self.root.setText(2,str(pre_symptoms))
+            self.root.setText(3,str(illness))
+            self.root.setText(4,"https://www.cup.edu.cn/medical/disease/commondisease/16419.htm")
+            # self.child_name.setText(0, str(name))
+            # self.child_name.setText(1, str(id))
+            # self.child_name.setText(2,str(pre_symptoms))
+            # self.child_name.setText(3,str(illness))
+            # self.child_name.setText(4,"https://www.cup.edu.cn/medical/disease/commondisease/16419.htm")
+            primary_screen = QApplication.primaryScreen()
+            sshot = primary_screen.grabWindow(0)
+            sshot.save('sshot.pdf')
+            self.init()
+            # self.center()
+            self.right_layout.setCurrentIndex(1)
 
     # display search page
     def show_search_page(self):
         self.init()
-        self.center()
+        # self.center()
         self.right_layout.setCurrentIndex(0)
 
 
     # set windows location as center
-    def center(self):
-        '''
-        获取桌面长宽
-        获取窗口长宽
-        移动
-        '''
-        screen = QDesktopWidget().screenGeometry()
-        size = self.geometry()
-        self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
+    # def center(self):
+    #     '''
+    #     获取桌面长宽
+    #     获取窗口长宽
+    #     移动
+    #     '''
+    #     screen = QDesktopWidget().screenGeometry()
+    #     size = self.geometry()
+    #     self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
 
     # 退出按钮 有信息框的提示　询问是否确认退出
     def quit_act(self):
